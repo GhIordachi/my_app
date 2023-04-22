651a161bd3f0db6999e9afdb3519f7808ffd24c5
GhIordachi <gheorgheiordachi@yahoo.com>
2023-04-22 11:57:00 +0300

    second

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -12,5 +13,7 @@ Rails.application.routes.draw do
 
   get 'ex_route', to: 'application#example', defaults: { format: :html }
   get 'ex_route.json', to: 'application#example', defaults: { format: :json } 
+
+  get 'sign_up', to: 'users#new'
 
 end
