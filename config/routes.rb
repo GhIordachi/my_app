772fe657a1cc03fe95461f94d6b1f376f3ae269c
GhIordachi <gheorgheiordachi@yahoo.com>
2023-04-29 11:51:09 +0300

    3rdLab

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -10,6 +11,7 @@ Rails.application.routes.draw do
   get 'goodbye', to: 'application#goodbye'
 
   get 'extra', to: 'application#extra'
+  get 'log_out', to:'extra#new'
 
   get 'ex_route', to: 'application#example', defaults: { format: :html }
   get 'ex_route.json', to: 'application#example', defaults: { format: :json } 
